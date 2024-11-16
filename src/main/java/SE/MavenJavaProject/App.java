65637e353bd524f9102f0c2d71d516413b8a8e75
prashanth <prashb987@gmail.com>
2024-11-16 16:20:46 +0530

    first java project

---
+++ src/main/java/SE/MavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package SE.MavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "hello this is prashanth " );
+    }
+}
